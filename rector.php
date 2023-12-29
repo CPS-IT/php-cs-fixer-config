79e97ac3044f9efaa1b859498af49af5cae174d9
Elias Häußler <e.haeussler@familie-redlich.de>
2023-12-29 11:45:50 +0100

    [TASK] Use PHPUnit specific rules for PHPStan and Rector

--- rector.php
+++ rector.php
@@ -22,6 +22,7 @@ declare(strict_types=1);
  */
 
 use Rector\Config\RectorConfig;
+use Rector\PHPUnit\Set\PHPUnitLevelSetList;
 use Rector\Set\ValueObject\LevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
@@ -32,5 +33,6 @@ return static function (RectorConfig $rectorConfig): void {
 
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
+        PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
     ]);
 };
