7f20b2365945c5e762b366f4667f6d6569c0d173
Elias Häußler <e.haeussler@familie-redlich.de>
2023-12-29 09:35:57 +0100

    Initial commit

---
+++ .php-cs-fixer.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Composer package "cpsit/php-cs-fixer-config".
+ *
+ * Copyright (C) 2023 Elias Häußler <e.haeussler@familie-redlich.de>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+use CPSIT\PhpCsFixerConfig;
+use PhpCsFixer\Config;
+
+$config = new Config();
+$config->getFinder()->in(__DIR__);
+
+$ruleset = new PhpCsFixerConfig\Rule\DefaultRuleset();
+$ruleset->apply($config);
+
+return $config;
